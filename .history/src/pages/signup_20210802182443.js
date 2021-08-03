2779fe1d1405967e3c9dfb4412ac6d12bbc69eb2
yasir <hellopleasantcodes@gmail.com>
2021-08-02 18:25:58 -0700

    done with the signup page

--- .history/src/pages/signup_20210802182443.js
+++ .history/src/pages/signup_20210802182443.js
@@ -2,7 +2,7 @@ import React, { useState, useContext } from 'react';
 import { useHistory } from 'react-router-dom';
 import { FirebaseContext } from '../context/firebase';
 import { Form } from '../components';
-import HeaderContainer from '../containers/header';
+import { HeaderContainer} from '../containers/header';
 import { FooterContainer } from '../containers/footer';
 import * as ROUTES from '../constants/routes';
 
