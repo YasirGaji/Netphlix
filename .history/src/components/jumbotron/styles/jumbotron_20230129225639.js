3640af234b4125d547a7a8496d14b3f3f853c596
Yasir Gaji <gajiyasir@gmail.com>
2023-01-30 07:03:18 +0100

    Back at it learnt how to pass props in styled components

--- .history/src/components/jumbotron/styles/jumbotron_20230129225639.js
+++ .history/src/components/jumbotron/styles/jumbotron_20230129225639.js
@@ -3,13 +3,6 @@ import styled from 'styled-components/macro';
 export const Inner = styled.div`
   display: flex;
   align-items: center;
-  flex-direction: ${({ direction }) => direction};
   justify-content: space-between;
   max-width: 1100px;
-  marging: auto;
-  width: 100%;
-
-  @media (max-width: 1000px) {
-    flex-direction: column;
-  }
 `;
