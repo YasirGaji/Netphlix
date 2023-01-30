704efc8c6864320a8a29d80909dd2365f099b7ff
Yasir Gaji <gajiyasir@gmail.com>
2023-01-30 19:50:50 +0100

    changed all netflix to netphlix

--- src/containers/footer.js
+++ src/containers/footer.js
@@ -5,6 +5,17 @@ export default function FooterContainer() {
   return (
     <Footer>
       <Footer.Title>Questions? Contact us.</Footer.Title>
+      <Footer.Break />
+
+      <Footer.Row>
+        <Footer.Column>
+          <Footer.Link href="#">FAQ</Footer.Link>
+          <Footer.Link href="#">Investor Relations</Footer.Link>
+          <Footer.Link href="#">Ways to Watch</Footer.Link>
+          <Footer.Link href="#">Corporate Information</Footer.Link>
+          <Footer.Link href="#">Netphlix Originals</Footer.Link>
+        </Footer.Column>
+      </Footer.Row>
     </Footer>
   )
 }
