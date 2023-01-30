a92ca8515fe486438283ef8a407407c374e13b04
Yasir Gaji <gajiyasir@gmail.com>
2023-01-30 19:54:02 +0100

    completed the footer

--- src/containers/footer.js
+++ src/containers/footer.js
@@ -4,7 +4,7 @@ import { Footer } from '../components'
 export default function FooterContainer() {
   return (
     <Footer>
-      <Footer.Title>Questions? Contact us.</Footer.Title>
+      <Footer.Title>Netphlix | Built by <Footer.Link href="https://www.yasirgaji.com">Yasir Gaji</Footer.Link>.</Footer.Title>
       <Footer.Break />
 
       <Footer.Row>
@@ -15,6 +15,29 @@ export default function FooterContainer() {
           <Footer.Link href="#">Corporate Information</Footer.Link>
           <Footer.Link href="#">Netphlix Originals</Footer.Link>
         </Footer.Column>
+
+        <Footer.Column>
+          <Footer.Link href="#">Help Center</Footer.Link>
+          <Footer.Link href="#">Jobs</Footer.Link>
+          <Footer.Link href="#">Terms of Use</Footer.Link>
+          <Footer.Link href="#">Contact Us</Footer.Link>
+        </Footer.Column>
+
+        <Footer.Column>
+          <Footer.Link href="#">Account</Footer.Link>
+          <Footer.Link href="#">Redeem Gift Cards</Footer.Link>
+          <Footer.Link href="#">Privacy</Footer.Link>
+          <Footer.Link href="#">Speed Test</Footer.Link>
+        </Footer.Column>
+
+        <Footer.Column>
+          <Footer.Link href="#">Media Center</Footer.Link>
+          <Footer.Link href="#">Buy Gift Cards</Footer.Link>
+          <Footer.Link href="#">Cookie Preferences</Footer.Link>
+          <Footer.Link href="#">Legal Notices</Footer.Link>
+        </Footer.Column>
+
+
       </Footer.Row>
     </Footer>
   )
