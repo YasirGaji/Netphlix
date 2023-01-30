797a0335768ad7f188b630a602dfd374c24b1349
Yasir Gaji <gajiyasir@gmail.com>
2023-01-30 19:43:32 +0100

    initiated the footer container

---
+++ src/containers/footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Footer } from '../components'
+
+export default function FooterContainer() {
+  return (
+    <Footer>
+      <Footer.Title>Questions? Contact us.</Footer.Title>
+    </Footer>
+  )
+}
