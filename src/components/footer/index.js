ae3063cdd53ec5cffe8ae2a7bf0fc2697470eb96
Yasir Gaji <gajiyasir@gmail.com>
2023-01-30 19:35:29 +0100

    added the footer styles

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -20,22 +20,22 @@ Footer.Row = function FooterRow({ children, ...restProps}) {
   return <Row {...restProps}>{children}</Row>
 }
 
-Footer.Column = function FooterRow({ children, ...restProps}) {
+Footer.Column = function FooterColumn({ children, ...restProps}) {
   return <Column {...restProps}>{children}</Column>
 }
 
-Footer.Link = function FooterRow({ children, ...restProps}) {
+Footer.Link = function FooterLink({ children, ...restProps}) {
   return <Link {...restProps}>{children}</Link>
 }
 
-Footer.Title = function FooterRow({ children, ...restProps}) {
+Footer.Title = function FooterTitle({ children, ...restProps}) {
   return <Title {...restProps}>{children}</Title>
 }
 
-Footer.Text = function FooterRow({ children, ...restProps}) {
+Footer.Text = function FooterText({ children, ...restProps}) {
   return <Text {...restProps}>{children}</Text>
 }
 
-Footer.Break = function FooterRow({ children, ...restProps}) {
+Footer.Break = function FooterBreak({ children, ...restProps}) {
   return <Break {...restProps}>{children}</Break>
 }
