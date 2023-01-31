f8967358d3c682d84a507f56c1f97c2cf8b95eb2
Yasir Gaji <gajiyasir@gmail.com>
2023-01-31 07:45:44 +0100

    fixed the accordion toggle issue

--- src/components/accordion/styles/accordion.js
+++ src/components/accordion/styles/accordion.js
@@ -2,22 +2,43 @@ import styled from "styled-components/macro";
 
 
 export const Container = styled.div`
+  display: flex;
+  border-bottom: 8px solid #222;
 
 `;
 
 export const Frame = styled.div`
-
+  margin-bottom: 40px;
 `;
 
 export const Inner = styled.div`
-
+  display: flex;
+  padding: 70px 45px;
+  flex-direction: column;
+  max-width: 815px;
+  margin: auto;
 `;
 
 export const Title = styled.h1`
+  font-size: 50px;
+  line-height: 1.1;
+  margin-top: 0;
+  margin-bottom: 8px;
+  color: white;
+  text-align: center;
 
+  @media (max-width: 600px) {
+    font-size: 35px;
+  }
 `;
 
 export const Item = styled.div`
+  color: white;
+  margin-bottom: 10px;
+
+  &:first-of-type {
+    margin-top: 3em;
+  }
 
 `;
 
--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -12,6 +12,7 @@ export default function FaqsContainer() {
           <Accordion.Body>{item.body}</Accordion.Body>
         </Accordion.Item>
       ))}
+      <Accordion.Item />
     </Accordion>
-  )
+  );
 }
