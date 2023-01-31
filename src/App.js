fa3956f01aae116de001eec3a6de4a51d4de6a65
Yasir Gaji <gajiyasir@gmail.com>
2023-01-31 07:01:59 +0100

    rendered the Faqs Container

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import FaqsContainer from './containers/faqs';
 import FooterContainer from './containers/footer';
 import JumbotronContainer  from './containers/jumbotron';
 
@@ -7,6 +8,7 @@ export default function App() {
   return( 
     <>
       <JumbotronContainer />
+      <FaqsContainer />
       <FooterContainer />
     </>
   )
