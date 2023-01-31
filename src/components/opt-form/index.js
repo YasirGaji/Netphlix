6a662d01532dd8fee197f80858233234ed0c4907
Yasir Gaji <gajiyasir@gmail.com>
2023-01-31 08:33:51 +0100

    created the opt form skeleton

---
+++ src/components/opt-form/index.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import {
+  Container, 
+  Input, 
+  Button, 
+  Break,
+  Text
+} from './styles/opt-form'
+
+export default function OptForm({children, ...restProps}) {
+  return (
+    <Container {...restProps}>{children}</Container>
+  )
+}
+
+OptForm.Input = function OptFormInput({...restProps}) {
+  return <Input {...restProps} />
+}
+
+OptForm.Button = function OptFormButton({children, ...restProps}) {
+  return (
+    <Button {...restProps}>
+      {children} <img src="/images/icons/chevron-right.png" alt="Try Now" />
+    </Button>
+  )
+}
+
+OptForm.Break = function OptFormBreak({...restProps}) {
+  return <Break {...restProps} />
+}
+
+OptForm.Text = function OptFormText({children, ...restProps}) {
+  return <Text {...restProps}>{children}</Text>
+}
---
+++ src/components/opt-form/styles/opt-form.js
@@ -0,0 +1,22 @@
+import styled from "styled-components/macro";
+
+export const Container = styled.div`
+
+`;
+
+export const Input = styled.input`
+
+`;
+
+export const Button = styled.button`
+
+`;
+
+export const Text = styled.p`
+
+`;
+
+export const Break = styled.div`
+
+`;
+
