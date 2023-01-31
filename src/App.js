ab400b432487ef4a4a799497ba92ad293dc2e4ba
Yasir Gaji <gajiyasir@gmail.com>
2023-01-31 10:15:31 +0100

    initialized my route constatnts

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import FaqsContainer from './containers/faqs';
 import FooterContainer from './containers/footer';
 import JumbotronContainer  from './containers/jumbotron';
+import * as ROUTES from './constants/routes';
 
 
 export default function App() {
---
+++ src/constants/routes.js
@@ -0,0 +1,8 @@
+export const HOME = '/';
+export const SIGN_UP = '/signup';
+export const SIGN_IN = '/signin';
+export const PASSWORD_FORGET = '/pw-forget';
+export const ACCOUNT = '/account';
+export const ADMIN = '/admin';
+export const ADMIN_DETAILS = '/admin/:id';
+export const BROWSE = '/browse';
