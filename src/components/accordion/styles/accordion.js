a0e83c792f1a1b640f89c3bc71b61bf129f384ab
Yasir Gaji <gajiyasir@gmail.com>
2023-02-01 08:57:07 +0100

    done with the pre-logged in homepage

--- src/components/accordion/styles/accordion.js
+++ src/components/accordion/styles/accordion.js
@@ -35,6 +35,9 @@ export const Title = styled.h1`
 export const Item = styled.div`
   color: white;
   margin-bottom: 10px;
+  max-width: 700px;
+  width: 100%;
+  margin: auto;
 
   &:first-of-type {
     margin-top: 3em;
