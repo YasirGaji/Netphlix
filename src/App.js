0f98f9c6ed6dae1fa6c2b029822da82c172d7cff
Yasir Gaji <gajiyasir@gmail.com>
2023-02-01 07:01:32 +0100

    initialised the pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Routes, Route } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
-import { Home } from './pages';
+import { 
+  Home,
+  Signin,
+  Signup,
+  Browse
+ } from './pages';
 
 
 export default function App() {
   return( 
     <Router>
     <Routes>
-      <Route exact path={ROUTES.HOME} element={<Home />}>  
-      </Route>
+      <Route exact path={ROUTES.HOME} element={<Home />} />
+      <Route exact path={ROUTES.SIGN_IN} element={<Signin />} /> 
+      <Route exact path={ROUTES.SIGN_UP} element={<Signup />} /> 
+      <Route exact path={ROUTES.BROWSE} element={<Browse />} /> 
     </Routes>
     </Router>
   )
