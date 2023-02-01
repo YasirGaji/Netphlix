574d4f36c8b141e96aa4421800af8b8e203f3be3
Yasir Gaji <gajiyasir@gmail.com>
2023-02-01 11:49:05 +0100

    initialized the firebase context in the src index

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import { render } from 'react-dom';
 import App from './App';
 import { GlobalStyles } from './global-styles';
 import 'normalize.css'
-import  firebase  from './lib/firebase.prod';
+import  { firebase }  from './lib/firebase.prod';
+import { FirebaseContext } from './context/firebase';
 
 render(
   <>
-    <GlobalStyles/>
-    <App />
+    <FirebaseContext.Provider value={{ firebase }}>
+      <GlobalStyles/>
+      <App />
+    </FirebaseContext.Provider>
   </>, 
   document.getElementById('root')
 );
