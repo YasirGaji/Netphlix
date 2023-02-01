fb96fc4b760b480763da26def4f943bac0ce8418
Yasir Gaji <gajiyasir@gmail.com>
2023-02-01 08:06:30 +0100

    added the logo

--- src/containers/header.js
+++ src/containers/header.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Header } from '../components'
 import * as ROUTES from '../constants/routes'
+import logo from '../logo.svg'
 
 export default function HeaderContainer({ children }) {
   return (
     <Header>
       <Header.Frame>
-        <Header.Logo to={ROUTES.HOME} alt="Netphlix" />
+        <Header.Logo to={ROUTES.HOME} src={logo} alt="Netphlix" />
         <Header.ButtonLink to={ROUTES.SIGN_IN}>Sign in</Header.ButtonLink>
       </Header.Frame>
       {children}
