006361df5754d8186dc87b4a5a2415ac3a53b595
Yasir Gaji <gajiyasir@gmail.com>
2023-02-02 09:01:14 +0100

    cleaned my code

--- src/helpers/routes.js
+++ src/helpers/routes.js
@@ -1,37 +1,10 @@
 import React from 'react'
 import { Navigate } from 'react-router-dom'
 
-// export function IsUserRedirect({ user, loggedInPath, children, ...rest }) {
-//   return (
-//     <Routes>
-//       <Route 
-//           {...rest}
-//           render={() => {
-//             if (!user) {
-//               return children
-//             }
-
-//             if (user) {
-//               return (
-//                 <Navigate 
-//                   to={{
-//                     pathname: loggedInPath
-//                   }}
-//                 />
-//               )
-//             }
-
-//             // return null
-//           }}/>
-//     </Routes>
-//   )
-// }
-
 export const ProtectedRoute = ({
   user,
   redirectPath = '/signin',
   children,
-  ...rest
 }) => {
   if (user) {
     return children;
@@ -52,7 +25,6 @@ export const IsUserRedirect = ({
   user,
   loggedInPath,
   children,
-  ...rest
 }) => {
   if (!user) {
     return children
