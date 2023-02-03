dc06de8c4de2c64b948518a94b901868f7d0afe6
Yasir Gaji <gajiyasir@gmail.com>
2023-02-03 06:58:23 +0100

    updated the footers user experience

--- src/components/footer/styles/footer.js
+++ src/components/footer/styles/footer.js
@@ -1,4 +1,5 @@
 import styled from "styled-components/macro";
+import { List } from "../../profiles/styles/profiles";
 
 export const Container = styled.div`
   display: flex;
@@ -45,6 +46,20 @@ export const Text = styled.p`
   font-size: 13px;
   color: #757575;
   margin-bottom: 40px;
+
+  &:hover {
+    transform: scale(1.3);
+    z-index: 99;
+    transition: all 0.3s ease-in-out;
+    color: #e50914;
+  }
+
+  &:hover > ${Link} {
+    transform: scale(1.3);
+    z-index: 99;
+    transition: all 0.3s ease-in-out;
+    color: #e50914;  
+  }
 `;
 
 export const Break = styled.div`
