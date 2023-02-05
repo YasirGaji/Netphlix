d5f09bb945d807b20345a0471dde5a97c76e6292
Yasir Gaji <gajiyasir@gmail.com>
2023-02-05 10:52:25 +0100

    accordion passed the test fully

--- src/_tests_/components/accordion.test.js
+++ src/_tests_/components/accordion.test.js
@@ -32,27 +32,33 @@ describe("<Accordion />", () => {
     expect(container.firstChild).toMatchSnapshot();
   });
 
-  // it("opens and closes the <Accordion /> component", () => {
-  //   const { container, queryByText } = render(
-  //     <Accordion>
-  //       <Accordion.Title>Frequently Asked Questions</Accordion.Title>
-  //       <Accordion.Frame>
-  //         {faqsData.map((item) => (
-  //           <Accordion.Item key={item.id}>
-  //             <Accordion.Header>{item.header}</Accordion.Header>
-  //             <Accordion.Body>{item.body}</Accordion.Body>
-  //           </Accordion.Item>
-  //         ))}
-  //       </Accordion.Frame>
-  //     </Accordion>
-  //   );
+  it("opens and closes the <Accordion /> component", () => {
+    const { container, queryByText } = render(
+      <Accordion>
+        <Accordion.Title>Frequently Asked Questions</Accordion.Title>
+        <Accordion.Frame>
+          {faqsData.map((item) => (
+            <Accordion.Item key={item.id}>
+              <Accordion.Header>{item.header}</Accordion.Header>
+              <Accordion.Body>{item.body}</Accordion.Body>
+            </Accordion.Item>
+          ))}
+        </Accordion.Frame>
+      </Accordion>
+    );
 
-  //   expect(queryByText("Netphlix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices.")).toBeFalsy();
-  //   fireEvent.click(queryByText("What is Netphlix?"));
-  //   expect(queryByText("Netphlix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices.")).toBeTruthy();
-  //   fireEvent.click(queryByText("What is Netphlix?"));
-  //   expect(queryByText("Netphlix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices.")).toBeFalsy();
-  //   expect(container.firstChild).toMatchSnapshot();
-  // });
+    const whatIsNetphlixBodyText = "Netphlix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices.";
+
+    const howMuchDoesNetphlixCostBodyText = "Watch Netphlix on your smartphone, tablet, smart TV, laptop or streaming device, all for one low fixed monthly fee. Plans start from ₦300.99 a month. No extra costs or contracts.";
+
+
+
+    expect(queryByText(whatIsNetphlixBodyText)).toBeFalsy();
+    fireEvent.click(queryByText("What is Netphlix?"));
+
+    expect(queryByText(howMuchDoesNetphlixCostBodyText)).toBeFalsy();
+    fireEvent.click(queryByText("How much does Netphlix cost?"));
+
+  });
 
 })
